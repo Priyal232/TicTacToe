fcf9aeb6847df773fab23f08b596fc923106530d
07pz1491 <pz1491@srmist.edu.in>
2026-04-21 13:13:34 +0530

    uc2

--- src/main/java/Tictactoe.java
+++ src/main/java/Tictactoe.java
@@ -1,20 +1,29 @@
-public class Tictactoe{
+import java.util.Random;
+
+public class Tictactoe {
 
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        Random random = new Random();
 
+        int toss = random.nextInt(2); // 0 or 1
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+        String currentPlayer;
+        char player1Symbol;
+        char player2Symbol;
+
+        if (toss == 0) {
+            currentPlayer = "Player 1";
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = "Player 2";
+            player1Symbol = 'O';
+            player2Symbol = 'X';
         }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("Toss Result:");
+        System.out.println(currentPlayer + " starts first");
+        System.out.println("Player 1 Symbol: " + player1Symbol);
+        System.out.println("Player 2 Symbol: " + player2Symbol);
     }
 }
