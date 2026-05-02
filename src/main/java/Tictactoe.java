746341b660881c05e6cd1ec6efe8ee42c0508c0b
07pz1491 <pz1491@srmist.edu.in>
2026-05-02 17:03:32 +0530

    uc9

--- src/main/java/Tictactoe.java
+++ src/main/java/Tictactoe.java
@@ -1,102 +1,59 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Tictactoe {
-
     static char[][] board = new char[3][3];
-    static boolean isHumanTurn = true;
     static boolean gameOver = false;
-    static char humanSymbol = 'X';
-    static char computerSymbol = 'O';
-    static Random rand = new Random();
-    static Scanner sc = new Scanner(System.in);
-
-    static {
-        resetBoard();
-    }
+    static char human = 'X';
+    static char computer = 'O';
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
 
     public static void main(String[] args) {
-        printBoardWithSlots();  // Initial board
+        initBoard();
+        boolean humanTurn = true;
+        while (!gameOver && !isBoardFull()) {
+            printBoard();
+            if (humanTurn) {
 
-        while (!gameOver) {
-            if (isHumanTurn) {
-                humanMoveSlot();
+                int[] move = getValidHumanMove();
+                makeMove(move[0], move[1], human);
             } else {
-                computerMoveSlot();
-            }
-            printBoardWithSlots();
-
-            if (checkWin(humanSymbol)) {
-                System.out.println("Human (X) wins!");
-                gameOver = true;
-            } else if (checkWin(computerSymbol)) {
-                System.out.println("Computer (O) wins!");
-                gameOver = true;
-            } else if (isDraw()) {
-                System.out.println("Draw!");
-                gameOver = true;
+                // Computer move (random valid)
+                int[] move = getComputerMove();
+                makeMove(move[0], move[1], computer);
+                System.out.println("Computer plays at (" + move[0] + "," + move[1] + ")");
             }
 
-            if (!gameOver) isHumanTurn = !isHumanTurn;
-        }
-        sc.close();
-    }
 
-    static void humanMoveSlot() {
-        System.out.print("Your move (1-9): ");
-        int slot = sc.nextInt();
-        while (!isValidSlot(slot)) {
-            System.out.print("Invalid/occupied (1-9): ");
-            slot = sc.nextInt();
-        }
-        int[] pos = slotToPosition(slot);
-        placeMove(pos[0], pos[1], humanSymbol);
-    }
-
-    static void computerMoveSlot() {
-        while (true) {
-            int slot = rand.nextInt(9) + 1;
-            if (isValidSlot(slot)) {
-                int[] pos = slotToPosition(slot);
-                placeMove(pos[0], pos[1], computerSymbol);
-                System.out.println("Computer picks slot " + slot);
-                return;
+            if (hasWon(humanTurn ? human : computer)) {
+                printBoard();
+                System.out.println((humanTurn ? human : computer) + " wins!");
+                gameOver = true;
+                break;
             }
+            humanTurn = !humanTurn;
         }
+
+        if (!gameOver) {
+            printBoard();
+            System.out.println("Draw!");
+        }
+        scanner.close();
     }
 
-    static boolean isValidSlot(int slot) {
-        if (slot < 1 || slot > 9) return false;
-        int[] pos = slotToPosition(slot);
-        return board[pos[0]][pos[1]] == '-';
-    }
-
-    static int[] slotToPosition(int slot) {
-        return new int[]{(slot - 1) / 3, (slot - 1) % 3};
-    }
-
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
-    }
-
-    static boolean checkWin(char symbol) {
+    static void initBoard() {
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
-            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) return true;
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
         }
-        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) return true;
-        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) return true;
-        return false;
     }
 
-    static boolean isDraw() {
-        for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if (board[i][j] == '-') return false;
-        return true;
-    }
-
-    static void printBoardWithSlots() {
+    static void printBoard() {
+        System.out.println("  0 1 2");
         for (int i = 0; i < 3; i++) {
-            System.out.print("  ");
+            System.out.print(i + " ");
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
             }
@@ -105,8 +62,59 @@ public class Tictactoe {
         System.out.println();
     }
 
-    static void resetBoard() {
-        for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) board[i][j] = '-';
-        isHumanTurn = true; gameOver = false;
+    static int[] getValidHumanMove() {
+        int row, col;
+        while (true) {
+            System.out.print("Your move (row 0-2, col 0-2): ");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+                return new int[]{row, col};
+            }
+            System.out.println("Invalid! Try again.");
+        }
+    }
+
+    static int[] getComputerMove() {
+
+        java.util.ArrayList<int[]> empty = new java.util.ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    empty.add(new int[]{i, j});
+                }
+            }
+        }
+
+        return empty.get(random.nextInt(empty.size()));
+    }
+
+    static void makeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+
+    static boolean hasWon(char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) return true;
+        }
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) return true;
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) return true;
+        return false;
+    }
+
+    static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') return false;
+            }
+        }
+        return true;
     }
 }
